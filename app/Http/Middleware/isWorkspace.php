04708449087f8ec5864d042f6b717a8d6a3b991f
bai cua Tham <thamntph43585@fpt.edu.vn>
2024-09-12 23:13:45 +0700

    sua iswworksp

--- app/Http/Middleware/isWorkspace.php
+++ app/Http/Middleware/isWorkspace.php
@@ -4,7 +4,9 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth; // Import Auth facade
+use Illuminate\Support\Facades\Auth;
+
+// Import Auth facade
 use Symfony\Component\HttpFoundation\Response;
 
 class isWorkspace
@@ -12,14 +14,16 @@ class isWorkspace
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Auth::check()&&!Auth::user()->hasWorkspace()) {
+        if (Auth::check() && !Auth::user()->hasWorkspace()) {
+
             if (!$request->is('workspaces/create')) {
+
                 return redirect('/workspaces/create');
             }
         }
