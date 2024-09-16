37124ebd99b03ac11a9e60c9e6ddc5c4b7fc7aa1
nguyengiangtuan <150132406+nguyengiangtuan@users.noreply.github.com>
2024-09-16 06:38:36 -0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,14 @@ class DatabaseSeeder extends Seeder
     {
         //  \App\Models\User::factory(10)->create();
 
+         \App\Models\User::factory(10)->create();
+         $this->call([
+            BoardSeeder::class,
+            CheckListSeeder::class,
+            TaskSeeder::class,
+            TemplateBoardSeeder::class,
+            WorkspaceSeeder::class,
+        ]);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
