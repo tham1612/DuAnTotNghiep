ea7f02a1afaf3c8171c48658ea9bdacf8c9d05c5
bai cua Tham <thamntph43585@fpt.edu.vn>
2024-09-17 10:46:13 +0700

    sua db

--- database/migrations/2024_09_05_062216_create_tasks_table.php
+++ database/migrations/2024_09_05_062216_create_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(\App\Models\Catalog::class)->constrained();
-            $table->string('title');
+            $table->string('text');
             $table->text('description')->nullable();
             $table->integer('position');
             $table->string('image')->nullable();
