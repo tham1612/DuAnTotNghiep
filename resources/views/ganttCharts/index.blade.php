6d07bea0fccab9cd3ea08740e59d34f08c9edd1b
nguyengiangtuan <nguyengiantuan0@gmail.com>
2024-09-19 01:44:13 -0700

    update gantt index

--- resources/views/ganttCharts/index.blade.php
+++ resources/views/ganttCharts/index.blade.php
@@ -28,6 +28,58 @@
     var dp = new gantt.dataProcessor("/api");
     dp.init(gantt);
     dp.setTransactionMode("REST");
+    // Override showLightbox to open your custom modal
+    gantt.showLightbox = function(taskId) {
+    openCustomModal(taskId); // Gọi modal tùy chỉnh
+};
+
+// Hàm để mở modal tùy chỉnh
+function openCustomModal(taskId) {
+    // Lấy phần tử modal dựa vào ID của nó
+    var modalElement = document.getElementById('detailCardModal');
+
+    if (modalElement) {
+        // Lấy task từ Gantt chart bằng taskId (giả sử bạn có taskId)
+        var task = gantt.getTask(taskId);
+
+        // Đưa dữ liệu vào input trong modal
+        var inputElement = document.getElementById("borderInput");
+        if (inputElement) {
+            inputElement.value = task.text;
+        } else {
+            console.error("Phần tử input không tìm thấy!");
+        }
+
+        // Mở modal Bootstrap
+        var modalInstance = new bootstrap.Modal(modalElement);
+        modalInstance.show();
+    } else {
+        console.error("Modal không tồn tại!");
+    }
+}
+Hàm để lưu thay đổi từ modal
+function saveTask() {
+    var taskId = gantt.getSelectedId();  // Lấy ID của tác vụ hiện tại
+    var task = gantt.getTask(taskId);  // Lấy thông tin tác vụ
+
+    // Lấy dữ liệu từ modal
+    var inputElement = document.getElementById("borderInput");
+    if (inputElement) {
+        task.text = inputElement.value;  // Cập nhật thông tin task từ modal
+    }
+
+    // Cập nhật task trong Gantt chart
+    gantt.updateTask(taskId);
+
+    // Đóng modal
+    var modalElement = document.getElementById('detailCardModal');
+    if (modalElement) {
+        var modalInstance = bootstrap.Modal.getInstance(modalElement);
+        if (modalInstance) {
+            modalInstance.hide();  // Ẩn modal
+        }
+    }
+}
 </script>
 </body>
 @endsection
