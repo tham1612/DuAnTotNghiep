c57d9ba1083a7dc76a11632fe89a6458b32a6ae4
bai cua Tham <thamntph43585@fpt.edu.vn>
2024-09-25 13:38:51 +0700

    updateTask3

--- database/migrations/2024_09_05_070729_create_task_members_table.php
+++ database/migrations/2024_09_05_070729_create_task_members_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('task_members', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(\App\Models\Task::class)->constrained();
-            $table->foreignIdFor(\App\Models\User::class)->constrained();
+            $table->foreignIdFor(\App\Models\Task::class)->unique()->constrained();
+            $table->foreignIdFor(\App\Models\User::class)->unique()->constrained();
             $table->boolean('follow')->default(false);
             $table->softDeletes();
             $table->timestamps();
