bd8492e40a0cbd98668b08ae9a4040f2128346d8
bai cua Tham <thamntph43585@fpt.edu.vn>
2024-10-03 12:49:18 +0700

    updateSTarDashboard

--- resources/views/layouts/masterMain.blade.php
+++ resources/views/layouts/masterMain.blade.php
@@ -163,6 +163,9 @@
 <script src="{{ asset('theme/assets/libs/feather-icons/feather.min.js') }}"></script>
 <script src="{{ asset('theme/assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
 <script src="{{ asset('theme/assets/js/plugins.js') }}"></script>
+<!--jquery cdn-->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
 @if (request()->is('b/*'))
     <!-- dragula init js -->
@@ -178,10 +181,6 @@
     {{--        <script src="{{ asset('theme/assets/js/pages/flag-input.init.js') }}"></script> --}}
 
     <script src="{{ asset('theme/assets/js/pages/project-list.init.js') }}"></script>
-
-    <!--jquery cdn-->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
     <!--select2 cdn-->
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
@@ -278,7 +277,7 @@
             alertElement.style.display = 'none';
         }
     }, 5000);
-    
+
     $(document).ready(function (){
         $.ajaxSetup({
             headers: {
