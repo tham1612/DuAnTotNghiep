681d4c84bacdf46aac48978ee169e8cdc3b5b60e
bai cua Tham <thamntph43585@fpt.edu.vn>
2024-10-04 23:21:06 +0700

    updateupdateTask

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,7 @@ Route::middleware(['auth', 'isWorkspace'])
         Route::get('/homes/dashboard', [BoardController::class, 'index'])->name('homes.dashboard');
 
         Route::get('/home', [HomeController::class, 'index'])->name('home');
-       
+
         Route::get('/chat', function () {
             return view('chat.index');
         })->name('chat');
@@ -100,6 +100,7 @@ Route::middleware(['auth', 'isWorkspace'])
         Route::get('/callback', [GoogleApiClientController::class, 'handleGoogleCallback']);
 
         Route::put('/tasks/updatePosition/{id}', [TaskController::class, 'updatePosition'])->name('update.position');
+        Route::put('/tasks/{id}/updateFolow', [TaskController::class, 'updateFolow'])->name('tasks.updateFolow');
 
     });
 
