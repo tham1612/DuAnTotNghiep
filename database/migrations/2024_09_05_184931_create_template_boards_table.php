7094c3aac2b4f52b17c1a256abdf876d6c68d257
dinhnguyet1110 <dinhnguyet11102004@gmail.com>
2024-11-09 11:39:27 +0700

    create table template, fix home

--- database/migrations/2024_09_05_184931_create_template_boards_table.php
+++ database/migrations/2024_09_05_184931_create_template_boards_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->string('image')->nullable();
+            $table->integer('complete')->default(0);
             $table->timestamps();
         });
     }
