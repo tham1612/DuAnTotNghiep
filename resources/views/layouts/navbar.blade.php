4627812591a46db4ef4e59dbb388f7facde38299
Thanh Thanh <nguyenthanhthanhgatay@gmail.com>
2024-11-14 16:51:32 +0700

    update

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,9 +5,9 @@
                 @php $memberIsStar = session('memberIsStar_' . $board->id); @endphp
                 <h4 class="fs-20 mx-3 mt-2 " id="2-name-board-{{ $board->id }}">{{ $board->name }}</h4>
                 <button type="button"
-                        class="btn avatar-xs mt-n1 p-0 favourite-btn
+                    class="btn avatar-xs mt-n1 p-0 favourite-btn
                     @if ($memberIsStar) active @endif"
-                        onclick="updateIsStar({{ $board->id }},{{ auth()->id() }})" id="is_star_{{ $board->id }}">
+                    onclick="updateIsStar({{ $board->id }},{{ auth()->id() }})" id="is_star_{{ $board->id }}">
                     <span class="avatar-title bg-transparent fs-15">
                         <i class="ri-star-fill fs-20 mx-2"></i>
 
@@ -15,12 +15,10 @@
                 </button>
                 <div class="mx-2 cursor-pointer" id="dropdownToggle" aria-expanded="false" data-bs-offset="10,20">
                     <i id="accessIcon_{{ $board->id }}"
-                       class="
-                        @if ($board->access == 'private')
-                        ri-lock-2-line fs-20 text-danger
+                        class="
+                        @if ($board->access == 'private') ri-lock-2-line fs-20 text-danger
                         @elseif($board->access == 'public')
-                            ri-shield-user-fill fs-20 text-primary
-@endif">
+                            ri-shield-user-fill fs-20 text-primary @endif">
                     </i>
                     <span id="accessText_{{ $board->id }}">
                         @if ($board->access == 'private')
@@ -31,15 +29,15 @@
                     </span>
                     <!-- Dropdown menu -->
                     <form id="updateBoardForm{{ $board->id }}"
-                          onsubmit="submitForm({{ $board->id }}); return false;">
+                        onsubmit="submitForm({{ $board->id }}); return false;">
                         <ul class="dropdown-menu dropdown-menu-md p-3" id="dropdownMenu"
                             style="width: 35%; display: none;">
                             @foreach (\App\Enums\AccessEnum::getValues() as $access)
                                 <li class="mb-2">
                                     <label class="dropdown-item w-100 d-flex align-items-start"
-                                           for="{{ $access }}Option">
+                                        for="{{ $access }}Option">
                                         <input class="form-check-input me-2 mt-1" type="radio" name="access"
-                                               id="Option{{ $board->id }}" value="{{ $access }}"
+                                            id="Option{{ $board->id }}" value="{{ $access }}"
                                             {{ $board->access == $access ? 'checked' : '' }}>
                                         <div>
                                             @if ($access == 'private')
@@ -57,8 +55,8 @@
                                                     Tất cả thành viên của Không gian làm việc TaskFlow có thể xem và sửa
                                                     bảng thông tin này.
                                                 </p>
-                                        @endif
-                                        {{--                                        </div> --}}
+                                            @endif
+                                            {{--                                        </div> --}}
                                     </label>
                                 </li>
                             @endforeach
@@ -85,50 +83,50 @@
             <ul class="nav nav-pills d-flex justify-content-between align-items-center" id="pills-tab" role="tablist">
                 <li class="nav-item" role="presentation">
                     <a class="nav-link {{ request()->get('type') == 'dashboard' ? 'active' : '' }}"
-                       href="{{ route('b.edit', ['viewType' => 'dashboard', 'id' => $board->id]) }}" role="tab"
-                       aria-controls="pills-home"
-                       aria-selected="{{ request()->get('type') == 'dashboard' ? 'true' : 'false' }}">
+                        href="{{ route('b.edit', ['viewType' => 'dashboard', 'id' => $board->id]) }}" role="tab"
+                        aria-controls="pills-home"
+                        aria-selected="{{ request()->get('type') == 'dashboard' ? 'true' : 'false' }}">
                         <i class="ri-dashboard-line fs-20"></i><span class="readonly fs-14"> Bảng điều khiển</span>
                     </a>
                 </li>
 
                 <li class="nav-item" role="presentation">
                     <a class="nav-link {{ request()->get('type') == 'board' ? 'active' : '' }}"
-                       href="{{ route('b.edit', ['viewType' => 'board', 'id' => $board->id]) }}" role="tab"
-                       aria-controls="pills-profile"
-                       aria-selected="{{ request()->get('type') == 'board' ? 'true' : 'false' }}">
+                        href="{{ route('b.edit', ['viewType' => 'board', 'id' => $board->id]) }}" role="tab"
+                        aria-controls="pills-profile"
+                        aria-selected="{{ request()->get('type') == 'board' ? 'true' : 'false' }}">
                         <i class="ri-table-line fs-20"></i> <span class="readonly fs-14"> Bảng </span>
                     </a>
                 </li>
                 <li class="nav-item" role="presentation">
                     <a class="nav-link {{ request()->get('type') == 'list' ? 'active' : '' }}"
-                       href="{{ route('b.edit', ['viewType' => 'list', 'id' => $board->id]) }}" role="tab"
-                       aria-controls="pills-list"
-                       aria-selected="{{ request()->get('type') == 'list' ? 'true' : 'false' }}">
+                        href="{{ route('b.edit', ['viewType' => 'list', 'id' => $board->id]) }}" role="tab"
+                        aria-controls="pills-list"
+                        aria-selected="{{ request()->get('type') == 'list' ? 'true' : 'false' }}">
                         <i class=" ri-list-check-2 fs-20"></i><span class="readonly fs-14"> Danh sách</span>
                     </a>
                 </li>
                 <li class="nav-item" role="presentation">
                     <a class="nav-link {{ request()->get('type') == 'ganttChart' ? 'active' : '' }}"
-                       href="{{ route('b.edit', ['viewType' => 'gantt', 'id' => $board->id]) }}" role="tab"
-                       aria-controls="pills-gantt"
-                       aria-selected="{{ request()->get('type') == 'ganttChart' ? 'true' : 'false' }}">
+                        href="{{ route('b.edit', ['viewType' => 'gantt', 'id' => $board->id]) }}" role="tab"
+                        aria-controls="pills-gantt"
+                        aria-selected="{{ request()->get('type') == 'ganttChart' ? 'true' : 'false' }}">
                         <i class="ri-calendar-todo-fill fs-20"></i><span class="readonly fs-14"> Lịch trình</span>
                     </a>
                 </li>
                 <li class="nav-item" role="presentation">
                     <a class="nav-link {{ request()->get('type') == 'table' ? 'active' : '' }}"
-                       href="{{ route('b.edit', ['viewType' => 'table', 'id' => $board->id]) }}" role="tab"
-                       aria-controls="pills-table"
-                       aria-selected="{{ request()->get('type') == 'table' ? 'true' : 'false' }}">
+                        href="{{ route('b.edit', ['viewType' => 'table', 'id' => $board->id]) }}" role="tab"
+                        aria-controls="pills-table"
+                        aria-selected="{{ request()->get('type') == 'table' ? 'true' : 'false' }}">
                         <i class="ri-layout-3-line fs-20"></i> <span class="readonly fs-14"> Bảng</span>
                     </a>
                 </li>
                 <li class="nav-item" role="presentation">
                     <a class="nav-link {{ request()->get('type') == 'calendar' ? 'active' : '' }}"
-                       href="{{ route('b.edit', ['viewType' => 'calendar', 'id' => $board->id]) }}" role="tab"
-                       aria-controls="pills-gantt"
-                       aria-selected="{{ request()->get('type') == 'calendar' ? 'true' : 'false' }}">
+                        href="{{ route('b.edit', ['viewType' => 'calendar', 'id' => $board->id]) }}" role="tab"
+                        aria-controls="pills-gantt"
+                        aria-selected="{{ request()->get('type') == 'calendar' ? 'true' : 'false' }}">
                         <i class=" ri-calendar-2-line fs-20"></i> <span class="readonly fs-14"> Lịch</span>
                     </a>
                 </li>
@@ -136,24 +134,24 @@
             <div class="col-auto ms-auto d-flex justify-content-end align-items-center">
                 <!--  bộ lọc -->
                 <div class="d-flex justify-content-center align-items-center p-1 cursor-pointer"
-                     data-bs-toggle="dropdown" aria-expanded="false" aria-haspopup="true">
+                    data-bs-toggle="dropdown" aria-expanded="false" aria-haspopup="true">
                     <i class="ri-filter-3-line fs-20"></i>
                     <span class="readonly fs-14">Bộ lọc</span>
                 </div>
                 <div class="fs-20 fw-lighter text-secondary">|</div>
                 <!-- setting bộ lọc -->
                 <form class="dropdown-menu dropdown-menu-md p-3" style="width: 35%;max-height: 450px" data-simplebar
-                      action="{{ route('b.edit', $board->id) }}" method="GET">
+                    action="{{ route('b.edit', $board->id) }}" method="GET">
                     {{--                    @csrf --}}
                     <input type="hidden" name="viewType" id="viewType"
-                           value="{{ request()->input('viewType') }}">
+                        value="{{ request()->input('viewType') }}">
                     <p class="text-center fs-15"><strong>Lọc</strong></p>
 
                     <!-- lọc tìm kiếm -->
                     <div class="mt-2">
                         <strong>Từ khóa</strong>
                         <input type="text" name="search" id="search-filter" placeholder="Nhập từ khóa..."
-                               class="form-control"/>
+                            class="form-control" />
                         <span class="fs-10">Tìm kiếm các thẻ, các thành viên, các nhãn và hơn thế nữa.</span>
                     </div>
 
@@ -161,14 +159,12 @@
                     <div class="mt-2">
                         <p><strong>Thành viên</strong></p>
                         <label for="no_member">
-                            <input type="checkbox" name="no_member"
-                                   id="no_member" @checked(isset($_GET['no_member']) ? $_GET['no_member'] : '') />
+                            <input type="checkbox" name="no_member" id="no_member" @checked(isset($_GET['no_member']) ? $_GET['no_member'] : '') />
                             <span>Không có thành viên</span>
                         </label>
-                        <br/>
+                        <br />
                         <label for="it_me">
-                            <input type="checkbox" name="it_me"
-                                   id="it-me" @checked(isset($_GET['it_me']) ? $_GET['it_me'] : '') />
+                            <input type="checkbox" name="it_me" id="it-me" @checked(isset($_GET['it_me']) ? $_GET['it_me'] : '') />
                             <span>Các thẻ chỉ định cho tôi</span>
                         </label>
                     </div>
@@ -177,17 +173,15 @@
                     <div class="mt-2">
                         <p><strong>Ngày hết hạn</strong></p>
                         <label for="no_date">
-                            <input type="checkbox" name="no_date"
-                                   id="no-date" @checked(isset($_GET['no_date']) ? $_GET['no_date'] : '') />
+                            <input type="checkbox" name="no_date" id="no-date" @checked(isset($_GET['no_date']) ? $_GET['no_date'] : '') />
                             <span>Không có ngày hết hạn</span>
                         </label>
-                        <br/>
+                        <br />
                         <label for="no_overdue">
-                            <input type="checkbox" name="no_overdue"
-                                   id="no-overdue" @checked(isset($_GET['no_overdue']) ? $_GET['no_overdue'] : '') />
+                            <input type="checkbox" name="no_overdue" id="no-overdue" @checked(isset($_GET['no_overdue']) ? $_GET['no_overdue'] : '') />
                             <span>Quá hạn</span>
                         </label>
-                        <br/>
+                        <br />
                         <label for="due_tomorrow">
                             <input type="checkbox" name="due_tomorrow" id="due-tomorrow"
                                 @checked(isset($_GET['due_tomorrow']) ? $_GET['due_tomorrow'] : '') />
@@ -199,23 +193,20 @@
                     <div class="mt-2">
                         <p><strong>Nhãn</strong></p>
                         <label for="no_tags" class="d-flex align-items-center">
-                            <input type="checkbox" name="no_tags"
-                                   id="no_tags" @checked(isset($_GET['no_tags']) ? $_GET['no_tags'] : '') />
+                            <input type="checkbox" name="no_tags" id="no_tags" @checked(isset($_GET['no_tags']) ? $_GET['no_tags'] : '') />
                             <i class="ri-price-tag-3-line mx-2 fs-20"></i>
                             <span class="rounded col-11">Không có nhãn</span>
                         </label>
                         @foreach ($board->tags as $tag)
                             <label for="{{ $tag->name }}_tags_{{ $tag->id }}"
-                                   class="d-flex align-items-center">
+                                class="d-flex align-items-center">
                                 <input type="checkbox" name="tags[]" value="{{ $tag->id }}"
-                                       id="{{ $tag->name }}_tags_{{ $tag->id }}"
-                                @if (isset($_GET['tags']))
-                                    @foreach ($_GET['tags'] as $check_tag)
+                                    id="{{ $tag->name }}_tags_{{ $tag->id }}"
+                                    @if (isset($_GET['tags'])) @foreach ($_GET['tags'] as $check_tag)
                                         @checked($check_tag == $tag->id)
-                                        @endforeach
-                                    @endif />
+                                        @endforeach @endif />
                                 <span class="mx-2 rounded p-2 col-11"
-                                      style="background-color: {{ $tag->color_code }}">
+                                    style="background-color: {{ $tag->color_code }}">
                                     {{ $tag->name }}
                                 </span>
                             </label>
@@ -243,11 +234,12 @@
                                     @endphp
                                     @if ($count < $maxDisplay)
                                         <a href="javascript: void(0);" class="avatar-group-item"
-                                           data-bs-toggle="tooltip" data-bs-placement="top"
-                                           title="{{ $boardMember['name'] }}">
+                                            data-bs-toggle="tooltip" data-bs-placement="top"
+                                            title="{{ $boardMember['name'] }}">
                                             @if ($boardMember['image'])
                                                 <img src="{{ asset('storage/' . $boardMember->image) }}"
-                                                    alt="" class="rounded-circle avatar-xs object-fit-cover" style="width: 35px;height: 35px">
+                                                    alt="" class="rounded-circle avatar-xs object-fit-cover"
+                                                    style="width: 35px;height: 35px">
                                             @else
                                                 <div class="avatar-xs" style="width: 35px;height: 35px">
                                                     <div class="avatar-title rounded-circle bg-light text-primary"
@@ -284,7 +276,7 @@
                     <!-- menu bảng -->
                     <div class="d-flex justify-content-center align-items-center p-2 cursor-pointer">
                         <i class="ri-list-settings-line fs-20" data-bs-toggle="offcanvas"
-                           data-bs-target="#settingBoard" aria-controls="offcanvasRight"></i>
+                            data-bs-target="#settingBoard" aria-controls="offcanvasRight"></i>
                     </div>
                 </section>
             </div>
@@ -302,7 +294,7 @@
             url: `/b/${boardId}/update`,
             method: 'PUT',
             data: formData,
-            success: function (response) {
+            success: function(response) {
                 console.log('chế độ bảng thay dổi thành công');
                 $('#dropdownMenu').hide();
                 if (formData.access === 'private') {
@@ -314,7 +306,7 @@
                     $('#accessText_' + boardId).text('Công khai');
                 }
             },
-            error: function (xhr) {
+            error: function(xhr) {
                 console.error('Lỗi xảy ra:', xhr.responseText);
             }
         });
